6da7bf1ed3042242fcddc99ba16ea63e038a74b0
eghbaldar <ali.eghbaldar2@gmail.com>
2024-05-08 18:18:02 +0330

    Added ConvertVideoMultiplieResolutions and GetVideoWidth

--- Endpoint.Site/Controllers/HomeController.cs
+++ Endpoint.Site/Controllers/HomeController.cs
@@ -1,14 +1,27 @@
-﻿using kingCompressVideo.Application.Services.VideoConverter;
+﻿using FFMpegCore;
+using kingCompressVideo.Application.Services.VideoConverter;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xabe.FFmpeg;
 
 namespace Endpoint.Site.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
+            string projectRoot = Directory.GetCurrentDirectory(); // Get the project's root directory
+            string inputFilePath = Path.Combine(projectRoot, "wwwroot", "originals/test.mp4"); // Combine paths to get full FFmpeg executable path
+            string ffmpegPath = Path.Combine(projectRoot, "wwwroot", "ffmpeg.exe"); // Replace with the path to your FFmpeg executable
+
+            Console.WriteLine(GetVideoWidth(inputFilePath, ffmpegPath));
+
             //ConverOneVideo();
-            ConvertWithList();
+            //ConvertWithList();
+            //ConvertVideoMultiplieResolutions();
             /////////////////////////////////////////////////////////////////////////////////////
             // NOTE
             // [1]
@@ -49,11 +62,89 @@ namespace Endpoint.Site.Controllers
             {
                 inputFiles.Add(Path.Combine(projectRoot, "wwwroot", $"originals/{video}"));
             }
- 
+
             string ffmpegPath = Path.Combine(projectRoot, "wwwroot", "ffmpeg.exe"); // Replace with the path to your ffmpeg executable
 
             ConvertWithList videoConverter = new ConvertWithList(ffmpegPath);
             await videoConverter.ConvertToMP4(inputFiles);
+        }
+        public async Task ConvertVideoMultiplieResolutions()
+        {
+            string projectRoot = Directory.GetCurrentDirectory(); // Get the project's root directory
+            string inputFilePath = Path.Combine(projectRoot, "wwwroot", "originals/test.mp4"); // Combine paths to get full FFmpeg executable path
+            string outputFilePath = Path.Combine(projectRoot, "wwwroot", "converted/converted.mp4"); // Combine paths to get full FFmpeg executable path   
+            string ffmpegPath = Path.Combine(projectRoot, "wwwroot", "ffmpeg.exe"); // Replace with the path to your ffmpeg executable
+
+            List<Tuple<int, int>> resolutions = new List<Tuple<int, int>>
+                {
+                new Tuple<int, int>(1920, 1080), // FULL HD 1920x1080
+                new Tuple<int, int>(1280, 720),  // HD 720p 1280x720
+                new Tuple<int, int>(854, 480)    // SD 854x480
+                };
+            ConvertOneVideoMultipleResolutions convertOneVideoMultipleResolutions = new ConvertOneVideoMultipleResolutions(ffmpegPath);
+            await convertOneVideoMultipleResolutions.ConvertToMP4(inputFilePath, outputFilePath, resolutions);
+        }
+
+        private string GetVideoInformation(string inputFilePath, string ffmpegPath)
+        {
+            string arguments = $"-i \"{inputFilePath}\"";
+
+            Process process = new Process();
+            process.StartInfo.FileName = ffmpegPath;
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.CreateNoWindow = true;
+
+            StringBuilder outputBuilder = new StringBuilder();
+
+            process.OutputDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    // Append the output to the StringBuilder
+                    outputBuilder.AppendLine(e.Data);
+                }
+            };
+
+            process.ErrorDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    // Append the error output to the StringBuilder
+                    outputBuilder.AppendLine(e.Data);
+                }
+            };
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            string output = outputBuilder.ToString();
+
+            // Extract the bitrate and resolution from the output
+            return output.ToString();
+
+        }
+        private int GetVideoWidth(string inputFilePath, string ffmpegPath)
+        {
+            // references:
+            // https://stackoverflow.com/questions/6758042/how-to-get-video-dimensions-using-ffmpeg
+            // https://askubuntu.com/questions/110264/how-to-find-frames-per-second-of-any-video-file
+            //
+
+            var re = new Regex("(\\d{2,4})x(\\d{2,4})");
+            Match m = re.Match(GetVideoInformation(inputFilePath,ffmpegPath));
+            int width = 1920; // default value to return
+            if (m.Success)
+            {
+                //int height = 0; //int.TryParse(m.Groups[2].Value, out height);
+                int.TryParse(m.Groups[1].Value, out width);                
+                return width;
+            }
+            return width;
         }
     }
 }
---
+++ kingCompressVideo.Application/Services/VideoConverter/ConvertOneVideoMultipleResolutions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kingCompressVideo.Application.Services.VideoConverter
+{    
+    public class ConvertOneVideoMultipleResolutions
+    {
+        private string ffmpegPath;
+        public ConvertOneVideoMultipleResolutions(string ffmpegPath)
+        {
+            this.ffmpegPath = ffmpegPath;
+        }
+        public async Task ConvertToMP4(string inputFilePath, string outputFilePath, List<Tuple<int, int>> resolutions)
+        {
+            foreach (var resolution in resolutions)
+            {
+                string arguments = $"-i \"{inputFilePath}\" -vf scale={resolution.Item1}:{resolution.Item2} -c:v libx264 -crf 23 -preset medium -c:a aac -b:a 128k -movflags +faststart \"{outputFilePath}_{resolution.Item1}x{resolution.Item2}.mp4\"";
+
+                Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n");
+                Console.WriteLine($"Converting {Path.GetFileName(inputFilePath)} to {Path.GetFileName(outputFilePath)}_{resolution.Item1}x{resolution.Item2}.mp4");
+                Console.WriteLine("-----------------------------------------------------------------------------");
+
+                Process process = new Process();
+                process.StartInfo.FileName = ffmpegPath;
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
+                process.ErrorDataReceived += (sender, e) => Console.WriteLine(e.Data);
+
+                process.Start();
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                await Task.Run(() => process.WaitForExit());
+            }
+
+        }
+    }    
+}
